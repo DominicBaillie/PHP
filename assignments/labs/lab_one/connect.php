6309ff4affba962e2d1baef8a1b1e6942cf69710
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-14 17:42:00 -0500

    Idk trynna make this work

--- assignments/labs/lab_one/connect.php
+++ assignments/labs/lab_one/connect.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-$host = "localhost";
-$db = "test_connection";
-$user = "root";
-$pass = "";
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$host = 'localhost';
+$db = 'test_connection';
+$user = 'root';
+$pass = '';
+$dsn = 'mysql:host=$host;dbname=$db;charset=utf8mb4';
 
 try 
 {

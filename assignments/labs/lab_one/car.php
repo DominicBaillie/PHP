e71a88ab14cdb840718146bed63977f3c8c7a948
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-14 18:10:00 -0500

    Noting

--- assignments/labs/lab_one/car.php
+++ assignments/labs/lab_one/car.php
@@ -1,14 +1,16 @@
 <?php
+// Probalbe unnecessary, but included the strict types
 declare(strict_types=1);
 
-
+// Basic car class
 class Car 
 {
+    // Public variable declarations
     public $make;
     public $model;
     public $year; 
 
-    public function __construct($make, $model, $year) 
+    public function construct($make, $model, $year) 
     {
         $this->make = $make;
         $this->model = $model;
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,5 @@
 <?php 
+// Import section, require header, footer and the connect php. Include the car php
 require "header.php"; 
 //echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
@@ -6,6 +7,8 @@ require_once "connect.php";
 
 include "car.php";
 
-$carObject = new car("Honda", "Civic", 2003);
+// Create the car object, containing the brand/make, the modle and the year
+$carObject = new car(" Honda", "Civic", 2003);
 
+// Call to getDescription function in echo
 echo $carObject->getDescription();

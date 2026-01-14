2c07bf078e17564d8cecf4e8612228ecefb7b656
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-14 15:14:45 -0500

    Big

--- 02/index.php
+++ 02/index.php
@@ -1,16 +1,39 @@
+<?php
 //1. Set Up & Start 
 
 
 //2. Code Commenting 
 
 
+
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
+$firthName = "Name"; //python style, but close it
+$age = 55;
+$isPerson = true; // lower bool
 
+echo "<p> Hello " . $firthName . ", you are " . $age . " years old. </p>";
+
+if ($isPerson)
+{
+    echo "<p> Congrats, you are a person! </p>";
+}
+else
+{
+    echo "<p> Sorry, you are not a person! </p>";
+}
 //4. Loosely Typed Language Demo
 
+$num1 = 1;
+$num2 = "10";
+
+function add($num1, $num2)
+{
+    return $num1 + (int)$num2;
+}
 
 //5. Strict Types & Types Hints
 
-
+declare(strict_types=1);
 //6. OOP with PHP 
+

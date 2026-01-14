f45ab62a5e555cdcd34b76598bed25aa46e3e760
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-14 17:27:53 -0500

    Include require

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -2,7 +2,7 @@
 require "header.php"; 
 //echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
-
+require_once "connect.php";
 
 include "car.php";
 

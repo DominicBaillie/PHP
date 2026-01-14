153c0af02c859d8f0c7f393820c7a637ac306c15
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-14 18:19:10 -0500

    Line?

--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -10,7 +10,7 @@ include "car.php";
 // Create the car object, containing the brand/make, the modle and the year
 $carObject = new car("Honda", "Civic", 2003);
 
-echo "\r"; // New line
+echo "\r\n"; // New line
 
 // Call to getDescription function in echo
 echo $carObject->getDescription();

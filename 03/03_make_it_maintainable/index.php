a6ddb4df245c0bcf7fed14e950e942a91ca24b7e
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-21 16:57:27 -0500

    Moving

---
+++ 03/03_make_it_maintainable/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'connect.php';
+require_once 'header.php';
+require_once 'footer.php';
+require_once 'data.php';
+
+/* What's the Problem? 
+    - PHP logic + HTML in one file
+    - Works, but not scalable
+    - Repetition will become a problem
+
+    How can we refactor this code so it’s easier to maintain?
+*/
+
+
+?>
+<ul>
+<?php foreach ($items as $item): ?>
+    <li><?= $item ?></li>
+<?php endforeach; ?>
+</ul>
+
+
--- 03/index.php
+++ 03/index.php
@@ -1,23 +1,5 @@
 <?php
-require_once 'connect.php';
-require_once 'header.php';
-require_once 'footer.php';
-require_once 'data.php';
 
-/* What's the Problem? 
-    - PHP logic + HTML in one file
-    - Works, but not scalable
-    - Repetition will become a problem
-
-    How can we refactor this code so it’s easier to maintain?
-*/
-
+echo "<h1> Hello World! </h1>"
 
 ?>
-<ul>
-<?php foreach ($items as $item): ?>
-    <li><?= $item ?></li>
-<?php endforeach; ?>
-</ul>
-
-

53caf0967cda6caa04be5db62124db4b94c4ddb8
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-21 16:43:25 -0500

    Connect

---
+++ 03/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+$host = "localhost";
+$db = "test_connection";
+$user = "root";
+$pass = "";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try 
+{
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch (PDOException $e) 
+{
+    die("Connection failed: " . $e->getMessage());
+}

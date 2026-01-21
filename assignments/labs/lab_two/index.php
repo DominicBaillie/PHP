c082d182855334d793bc7cd8bdec8a21cfee0f96
Dominic Baillie <dominicbaillie8@gmail.com>
2026-01-21 17:12:57 -0500

    Change

--- assignments/labs/lab_two/index.php
+++ assignments/labs/lab_two/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'connect.php';
-require_once 'header.php';
-require_once 'footer.php';
-require_once 'data.php';
+require 'header.php';
+require 'footer.php';
+require 'data.php';
 
 /* What's the Problem? 
     - PHP logic + HTML in one file

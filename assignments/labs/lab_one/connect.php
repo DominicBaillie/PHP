fb3a474794898c626724fab2856d8d09a6b6a2dd
Dominic Baillie <dominicbaillie8@gmail.com>
2026-02-04 16:14:25 -0500

    Doningx2

--- assignments/labs/lab_one/connect.php
+++ assignments/labs/lab_one/connect.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 $host = 'localhost';
 $db = 'test_connection';
 $user = 'root';
-
+$pass = 'password';
 //I used a costum port, so needed to learn a new $dsn (I was really struggling getting this to work)
 $dsn = "mysql:host=$host;port=3307;dbname=$db;charset=utf8mb4";
 
@@ -14,7 +14,7 @@ $dsn = "mysql:host=$host;port=3307;dbname=$db;charset=utf8mb4";
 try 
 {
     // Create new PDO
-    $pdo = new PDO($dsn, $user);
+    $pdo = new PDO($dsn, $user, $pass);
     // Set error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // Success message

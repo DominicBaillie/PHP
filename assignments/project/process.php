5b77acbfc24f1d360195973809c9921a23f8cbd1
dominicbaillie <dominicbaillie8@gmail.com>
2026-02-15 10:14:54 -0500

    ...

--- assignments/project/process.php
+++ assignments/project/process.php
@@ -13,7 +13,11 @@ $bio  = trim(filter_input(INPUT_POST, 'bio', FILTER_SANITIZE_SPECIAL_CHARS));
 $delete = filter_input(INPUT_POST, 'delete', FILTER_SANITIZE_SPECIAL_CHARS);
 $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 # Make into a list
+$skills = str_replace("\r\n", "\n", $skills);
 $skillsarray = explode("\n", $skills);
+$skillsarray = array_map('trim', $skillsarray);
+$skillsarray = array_filter($skillsarray);
+
 $errors = [];
 
 # Validation / I don't expect a name and such if it's being deleted
@@ -103,7 +107,7 @@ $stmt->execute();
             <!-- List skills provided -->
             <h4>Skills:</h4>
             <?php foreach ($skillsarray as $skill) { ?>
-                <p>Skill: <?php echo $skill; ?></p>
+                <ul><li>Skill: <?php echo $skill; ?></li></ul>
             <?php } ?>
             <p class="mt-3">
                 <a href="update.php">View Submissions</a>
